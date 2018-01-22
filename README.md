0a83a733b4636b0e8fb288702c1b44b74b9770d3
Marcus Gustafsson <marcusgu@hotmail.com>
2018-01-22 21:16:01 +0100

    second commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# Simple Node.js Server
+
+Without using any additional frameworks. Http module is already included in Node.js.
+
+## Try it out locally
+
+```
+git clone https://github.com/marcusgsta/simple-node-server.git
+
+cd simple-node-server
+
+npm start
+```
+
+Then open web browser to http://127.0.0.1:9000/ to see the result.
+
+Example is from [https://nodejs.org/api/synopsis.html](https://nodejs.org/api/synopsis.html)
--- app.js
+++ app.js
@@ -1,8 +1,18 @@
-let http = require("http");
-let port = 9000;
+const http = require("http");
+const port = 9000;
+const hostname = '127.0.0.1';
 
-http.createServer(function(req, res) {
-    res.writeHead(200, { "Content-Type": "text/html" });
-    res.write("<html><body><h1>Hello World!</h1></body></html>");
-    res.end();
-}).listen(port);
+const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    // res.setHeader('Content-Type', 'text/plain');
+    res.setHeader('Content-Type', 'text/html')
+    res.end('<html><body><h1>Hello World</h1></body></html>');
+
+    // res.writeHead(200, { "Content-Type": "text/html" });
+    // res.write("<html><body><h1>Hello World!</h1></body></html>");
+    // res.end();
+});
+
+server.listen(port, hostname, () => {
+    console.log('Server running at http://' + hostname + ':' + port + '/')
+});
